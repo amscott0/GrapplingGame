fc558534f9a295bd6c7e2533b5ed46aab333d56f
amscott0 <absc3094@colorado.edu>
2022-12-11 00:08:54 -0700

    Added grappling gun, added some grappling functionality.

--- Assets/Scripts/GrapplingBehavior.cs
+++ Assets/Scripts/GrapplingBehavior.cs
@@ -9,18 +9,38 @@ using System;
 
 public class GrapplingBehavior
 {
-    bool prevClicked = false;
-    StarterAssetsInputs _input;
+    private bool prevClicked = false;
+    private StarterAssetsInputs _input;
+    private LineRenderer _lr;
 
-    // Start is called before the first frame update
-    public GrapplingBehavior(StarterAssetsInputs inputs){
+    private Vector3 grapplingPos;
+
+    private Transform _camera, _playerPos;
+
+    
+    public GrapplingBehavior(StarterAssetsInputs inputs, LineRenderer lr, Transform plyr,Transform cmra){
         _input = inputs;
+        _lr = lr;
+        _camera = cmra;
+        _playerPos = plyr;
     }
-    // Update is called once per frame
+    
     public Vector3 grapple(){
-        Debug.Log(_input.grapple);
-        return Vector3.zero;
-        
+        RaycastHit hit;
+        if(Physics.Raycast(_camera.position, _camera.forward, out hit, 100f)){
+            grapplingPos = hit.point;
+        }
+        if(_input.grapple){
+            _lr.SetPosition(0, _playerPos.position);
+            _lr.SetPosition(1, grapplingPos);
+            Debug.Log("grappled to " + grapplingPos);
+            return Vector3.Normalize(grapplingPos);
+        }
+        else{
+            _lr.SetPosition(0, _playerPos.position);
+            _lr.SetPosition(1, _playerPos.position);
+            return Vector3.zero;
+        }
     }
     
 
